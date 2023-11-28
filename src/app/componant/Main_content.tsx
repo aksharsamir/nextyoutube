9db1e8b687f3ea4ca8ed1aae9b91499209954bd2
aksharsamir <136631095+aksharsamir@users.noreply.github.com>
2023-11-28 10:35:24 +0000

    setup api

--- src/app/componant/Main_content.tsx
+++ src/app/componant/Main_content.tsx
@@ -1,21 +1,27 @@
-'use client'
+// 'use client'
 import Img from "next/image"
 
 export default async function Main_content() {
 
-    // const apikey = process.env.YOUTUBE_API_KEY
-    const apikey = "AIzaSyBH0mripjJ5ZHdsxVH0COw0XnJfPKmYItA"
+    const apikey = process.env.YOUTUBE_API_KEY
+    const youtube_url = process.env.YOUTUBE_URL
+    const channel_id = process.env.YOUTUBE_ID
+    // const apikey = "AIzaSyBH0mripjJ5ZHdsxVH0COw0XnJfPKmYItA"
     // const apikey = "AIzaSyD5BVVqW1YYfVjqVYJ_U661fRQjxp-JSvQ"
-    const channel_id = "UCClMPKqtJ1LbRBCDP948g5q"
-    const youtube_url = "https://www.googleapis.com/youtube/v3/search"
 
-
-    const data = await fetch(`${youtube_url}?key=${apikey}&channelID${channel_id}&order=date&part=snippet`);
-    const VideoData = await data.json();
-    console.log(VideoData);
+    const fetch_data = await fetch(`${youtube_url}?key=${apikey}&channelID${channel_id}&order=date&part=snippet`);
+    const json_Data = await fetch_data.json();
+    const video_data = json_Data.items;
 
     return (
         <div className='mt-2 flex flex-wrap justify-around '>
+            {
+                video_data.map((e:string[]) => {
+                    // e.map((y)=>{
+                        console.log(e)
+                    // });
+                })
+            }
             <div>
                 <div>
                     <Img src="/images/1demo.jpg" width={100} height={100} className='rounded-3xl' alt="logo" />
